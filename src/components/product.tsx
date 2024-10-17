41019f5d1a6d657b1e9138025751629f427f931c
M40 <modev.404@gmail.com>
2024-10-17 14:20:47 +0300

    products fetch & display

---
+++ src/components/product.tsx
@@ -0,0 +1,83 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ShoppingCart } from "lucide-react";
+
+type Product = {
+  id: number;
+  title: string;
+  image: string;
+  price: number;
+  description: string;
+  brand: string;
+  model: string;
+  color: string;
+  category: string;
+  discount: number;
+};
+
+export default function Product({ product }: { product: Product }) {
+  const discountedPrice = product?.price * (1 - product?.discount / 100);
+
+  return (
+    <Card className="w-full max-w-sm overflow-hidden">
+      <CardHeader className="p-0">
+        <div className="relative w-full">
+          <div className="flex items-center justify-center">
+            <img
+              width={200}
+              height={200}
+              src={product?.image}
+              alt={product?.title}
+              className="transition-transform duration-300 ease-in-out hover:scale-105"
+            />
+          </div>
+          {product?.discount > 0 && (
+            <Badge variant="outline" className="absolute top-2 right-2">
+              {product?.discount}% OFF
+            </Badge>
+          )}
+        </div>
+      </CardHeader>
+      <CardContent className="py-4">
+        <CardTitle className="text-lg font-semibold line-clamp-1">
+          {product?.title}
+        </CardTitle>
+        <CardDescription className="mt-1 text-sm text-muted-foreground">
+          {product?.model}
+        </CardDescription>
+        <div className="flex items-baseline gap-2 mt-2">
+          <span className="text-2xl font-bold">
+            ${product?.discount > 0 ? discountedPrice : product?.price}
+          </span>
+          {product?.discount > 0 && (
+            <span className="text-sm line-through text-muted-foreground">
+              ${product?.price}
+            </span>
+          )}
+        </div>
+        <p className="mt-2 text-sm text-muted-foreground line-clamp-2">
+          {product?.description}
+        </p>
+        <div className="flex items-center gap-2 mt-2">
+          <Badge variant="secondary">{product?.category}</Badge>
+          <Badge variant="outline" className="text-primary">
+            {product?.color}
+          </Badge>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button>
+          <ShoppingCart className="w-4 h-4 mr-2" /> Add to Cart
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ src/components/products.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import useProducts from "@/store/productsSlice";
+import Product from "@/components/product";
+import ProductSkeleton from "./skeleton";
+
+const Products = () => {
+  const { products, isLoading, fetchProducts, error } = useProducts();
+
+  useEffect(() => {
+    fetchProducts(1);
+  }, []);
+
+  if (error) return <div>{JSON.stringify(error)}</div>;
+
+  return isLoading ? (
+    <div className="container grid grid-cols-3 gap-4 p-3 mx-auto">
+      {Array.from({ length: 12 }).map((_, index) => (
+        <ProductSkeleton key={index} />
+      ))}
+    </div>
+  ) : (
+    <div className="container grid grid-cols-3 gap-4 p-3 mx-auto">
+      {products.map((product) => (
+        <Product product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default Products;
