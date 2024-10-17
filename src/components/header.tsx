01bb0c5f9066f2b8149e013a81d6adbae3624490
M40 <modev.404@gmail.com>
2024-10-17 14:47:00 +0300

    update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,16 @@
+import clsx from "clsx";
 import { ShoppingBag, ShoppingCart } from "lucide-react";
+import useScroll from "./useScroll";
 
 function Header() {
+  const isScrolled = useScroll(50);
   return (
-    <header className="fixed top-0 left-0 right-0 z-10 p-4 bg-white shadow-sm">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 right-0 z-10 p-4 bg-white/0 shadow-sm",
+        { "border-b border-gray-200 bg-white/50 backdrop-blur-xl": isScrolled }
+      )}
+    >
       <nav className="container flex flex-row justify-between mx-auto">
         <ShoppingBag />
         <ShoppingCart />
